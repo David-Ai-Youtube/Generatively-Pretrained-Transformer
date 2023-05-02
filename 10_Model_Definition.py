3d5135cd3cab7ad604de9b6d20a3c815629273d8
David Ai <124640189+David-Ai-Youtube@users.noreply.github.com>
2023-05-02 10:28:14 -0400

    Update and rename 10_Model_Definition.txt to 10_Model_Definition.py

---
+++ 10_Model_Definition.py
@@ -0,0 +1,98 @@
+#The TransformerModel class defines the entire transformer model architecture by combining multiple Block layers along with the embedding layer and the final linear output layer.
+
+#The __init__ method initializes the hyperparameters and creates the embedding layer, as well as a list of Block layers. The forward method then takes the input tokens, passes them through the embedding layer, applies the series of Block layers, and finally passes the output through the final linear layer to generate the predicted output.
+
+#Specifically, the __init__ method defines the following components:
+
+    #self.encoder: The embedding layer which maps input tokens to the vector space. The dimension of the vector space is defined by the n_embd hyperparameter.
+
+    #self.blocks: A list of Block layers. The number of blocks is defined by the n_layer hyperparameter.
+
+    #self.decoder: The final linear layer which maps the output of the last block to the output dimension. The output dimension is defined by the number of unique characters in the input data.
+
+#The forward method takes the following steps:
+
+    #Pass the input tokens through the embedding layer to get the input embeddings.
+
+    #Scale the input embeddings by multiplying with the square root of the embedding dimension.
+
+    #Pass the scaled embeddings through each block in self.blocks in sequence. Each block outputs a new set of embeddings.
+
+    #Pass the output embeddings of the last block through the decoder to obtain the final output.
+
+    #Return the final output.
+
+#This way, the TransformerModel class defines the entire transformer model architecture that can be trained and used for prediction.
+
+class Head(nn.Module):
+    """ one head of self-attention """
+
+    def __init__(self, head_size):
+        super().__init__()
+        self.key = nn.Linear(n_embd, head_size, bias=False)
+        self.query = nn.Linear(n_embd, head_size, bias=False)
+        self.value = nn.Linear(n_embd, head_size, bias=False)
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        B,T,C = x.shape
+        k = self.key(x)   # (B,T,C)
+        q = self.query(x) # (B,T,C)
+        # compute attention scores ("affinities")
+        wei = q @ k.transpose(-2,-1) * C**-0.5 # (B, T, C) @ (B, C, T) -> (B, T, T)
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (B, T, T)
+        wei = F.softmax(wei, dim=-1) # (B, T, T)
+        wei = self.dropout(wei)
+        # perform the weighted aggregation of the values
+        v = self.value(x) # (B,T,C)
+        out = wei @ v # (B, T, T) @ (B, T, C) -> (B, T, C)
+        return out
+
+class MultiHeadAttention(nn.Module):
+    """ multiple heads of self-attention in parallel """
+
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(n_embd, n_embd)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.dropout(self.proj(out))
+        return out
+
+class FeedFoward(nn.Module):
+    """ a simple linear layer followed by a non-linearity """
+
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, 4 * n_embd),
+            nn.ReLU(),
+            nn.Linear(4 * n_embd, n_embd),
+            nn.Dropout(dropout),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+class Block(nn.Module):
+    """ Transformer block: communication followed by computation """
+
+    def __init__(self, n_embd, n_head):
+        # n_embd: embedding dimension, n_head: the number of heads we'd like
+        super().__init__()
+        head_size = n_embd // n_head
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.ffwd = FeedFoward(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
+
+    def forward(self, x):
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
+        return x
+
