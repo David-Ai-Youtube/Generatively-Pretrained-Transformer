289021d946b2b7d118e0f9bf3ab53ecd45c8b340
David Ai <124640189+David-Ai-Youtube@users.noreply.github.com>
2023-05-02 09:45:08 -0400

    Update 3_Reading_Data.py

--- 3_Reading_Data.py
+++ 3_Reading_Data.py
@@ -3,14 +3,14 @@
 #The code reads input data from a text file named 'input.txt'. The text file is read using Python's built-in function, open(), and the entire text is stored as a string in a variable called text. 
 #This text is then used to create a mapping between characters and integers, which is required for feeding the data into the model.
 
-The mapping between characters and integers is created using two dictionaries: stoi and itos. stoi is a dictionary that maps each unique character in the input text to a unique integer value, 
+#The mapping between characters and integers is created using two dictionaries: stoi and itos. stoi is a dictionary that maps each unique character in the input text to a unique integer value, 
 #and itos is a dictionary that maps each unique integer value back to its corresponding character.
 
-Once the mapping between characters and integers has been created, the input text is encoded as a list of integers using the encode() function. 
+#Once the mapping between characters and integers has been created, the input text is encoded as a list of integers using the encode() function. 
 #This list of integers is then split into training and validation sets using a simple slicing operation. 
 #The first 90% of the data is used for training, and the remaining 10% is used for validation.
 
-Overall, this process of reading and preprocessing data is an important step in preparing the data for training the model. 
+#Overall, this process of reading and preprocessing data is an important step in preparing the data for training the model. 
 #By converting the input text into a format that can be processed by the model, it allows the model to learn 
 #patterns and relationships in the data that can be used for generating new text.
 
